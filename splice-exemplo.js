cda88e836a283f60f920504d3b6f832c1ff33b42
LuizBottoli <bottoli533@gmail.com>
2026-09-09 22:23:13 -0300

    Usando Splice e Pop

---
+++ splice-exemplo.js
@@ -0,0 +1,7 @@
+const frutas = ["maçã", "banana", "laranja", "uva", "abacaxi"];
+
+const frutasRemovidas = frutas.splice(0, 2);
+
+console.log("Frutas removidas:", frutasRemovidas);
+
+console.log("Frutas restantes:", frutas);
